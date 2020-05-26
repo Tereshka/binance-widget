855a48a03966be3cd4ae4f65996584e2e7c88b90
Tereshka <tereshka07@gmail.com>
2020-05-27 01:06:01 +0900

    Updates

---
+++ precache-manifest.59a046178027d4a7e5b79fb740286b9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab086961e21d799b7c894e8468e7f57",
+    "url": "/binance-widget/index.html"
+  },
+  {
+    "revision": "30497d7ee82ac8ff5eea",
+    "url": "/binance-widget/static/js/2.2f896948.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/binance-widget/static/js/2.2f896948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d767bcf59e3f04216f",
+    "url": "/binance-widget/static/js/main.709fd70c.chunk.js"
+  },
+  {
+    "revision": "81840151d28e8e18f1db",
+    "url": "/binance-widget/static/js/runtime-main.9793053f.js"
+  }
+]);
